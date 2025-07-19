f897c642bed4ec197346101aba2eec790f666986
cauchy1988 <tangchao52121@163.com>
2025-07-19 15:01:39 +0800

    add files

--- src/solution.py
+++ src/solution.py
@@ -4,14 +4,19 @@
 # @Author  : Tang Chao
 # @File    : solution.py
 # @Software: PyCharm
+import abc
 import meta
 
 
 class Solution:
+	@abc.abstractmethod
+	def __init__(self, meta_obj: meta):
+		pass
+	
+	
+class PDWTWSolution(Solution):
 	def __init__(self, meta_obj: meta):
 		self.metaObj = meta_obj
 		# vehicle_id -> path
 		self.paths = {}
 		self.request_bank = set([request_id for request_id, _ in meta_obj.requests])
-		self.unvisited_node_ids = set([])
-		
